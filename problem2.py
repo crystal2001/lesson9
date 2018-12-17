9aed204861bf28c034b795a938cf57de96eb5c10
crystal2001 <43784510+crystal2001@users.noreply.github.com>
2018-12-17 11:26:22 -0500

    First

---
+++ problem2.py
@@ -0,0 +1,13 @@
+print('-' * 75)
+print('Locker Bot:')
+print()
+password = input('What is the password? ')
+if password == 'Open Sesame':
+	print('Password is valid!')
+	print('Locker Opening')
+else:
+	print('Password Invaild...enabling laser defense system, please stand down.')	
+	print('Pew! Pew! Pew!')
+	print('Pew! Pew! Pew!')
+
+print('-' * 75)
---
+++ problem3.py
@@ -0,0 +1,13 @@
+print('-' * 75)
+print('Alien Greeter Bot:')
+print()
+
+planet = input('What planet are you from? ')
+if planet == 'Earth':
+	print('Dammmmmnnnnn, your face is so ugly I thought you were an alien. Anyway...have a good day')
+
+else:
+	print('Welcome to Earth, please do not kill us!')
+
+
+print('-' * 75)
